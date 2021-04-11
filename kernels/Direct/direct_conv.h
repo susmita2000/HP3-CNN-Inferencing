46a94dba092757b2866f51b80daf94a2b7167e4a
susmita2000 <50179105+susmita2000@users.noreply.github.com>
2021-04-12 00:35:51 +0530

    Update direct_conv.h

--- kernels/Direct/direct_conv.h
+++ kernels/Direct/direct_conv.h
@@ -20,5 +20,11 @@ namespace Direct {
 					          float* d_weights,int batchsize_of_data, int input_height, int input_width, float* d_input, float &conv_time, float& overhead_time);
 }
 
+namespace DirectShared {
+
+	float* passforward(int out_channels, int input_channels, int kernel_height, int kernel_width, int padding, int stride, 
+					          float* d_weights,int batchsize_of_data, int input_height, int input_width, float* d_input, float &conv_time, float& overhead_time);
+}
+
 #endif
 
